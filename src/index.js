98ef10240a72fbfe1e22bb214dced8ae584c8cd9
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-06-18 14:24:25 -0700

    first prototype, lucky number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+var userObj = {
+    name: "Stephanie",
+    luckyNumber: luckyNumber(),
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
+
+function luckyNumber() {
+    return Math.floor(Math.random() * 1000);
+}
+
+function greeting(userObj) {
+   return <div>
+            <h1>Hello {userObj.name} </h1>
+            <h2 className="text-muted"> Your lucky number is {userObj.luckyNumber}</h2>
+         </div>
+}
+
