e88405c0d997a5bbea942d9862d5f3b848b8d6ff
StephanieLDR <stephanie.c.lacina@gmail.com>
2018-06-22 12:30:22 -0700

    forms and inputs incomplete

---
+++ src/data/contacts.js
@@ -0,0 +1,27 @@
+export default [
+    {
+       firstName: "Tofutti",
+       lastName: "Cline",
+       phone: "310-555-1234",
+       email: "missyfucci@yahoo.com"
+    },
+
+    {
+        firstName: "Schwee",
+        lastName: "Pittarelli",
+        phone: "310-555-5678",
+        email: "schwee@yahoo.com"
+    },
+    {
+        firstName: "Boogers",
+        lastName: "Lacina",
+        phone: "310-555-9876",
+        email: "boogers@yahoo.com"
+    },
+    {
+        firstName: "Bow",
+        lastName: "Diddly",
+        phone: "310-555-6453",
+        email: "bow@yahoo.com"
+    }
+];
